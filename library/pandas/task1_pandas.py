6614ac4be42267f95e5c3025bf61041d832d07ec
Harshil <sorathiyaharshil97@gmail.com>
2026-06-08 14:58:59 +0530

    .

--- library/pandas/task1_pandas.py
+++ library/pandas/task1_pandas.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 #1.Create a Pandas Series from a list.
 b = [10, 20, 30, 40, 50]
+
 series = pd.Series(b)
+
 print(series)
 
---
+++ library/pandas/task3_pandas.py
@@ -0,0 +1,7 @@
+#3.Display the first 5 rows of a DataFrame
+import pandas as pd
+data = {'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+        'Age': [25, 30, 35, 40, 45],
+        'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']}
+df = pd.DataFrame(data)
+print(df.head(3))       
