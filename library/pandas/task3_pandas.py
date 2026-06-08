64dff381640a4e0fcd84994c0fc2208bdc763b55
Harshil <sorathiyaharshil97@gmail.com>
2026-06-08 15:00:27 +0530

    .

--- library/pandas/task3_pandas.py
+++ library/pandas/task3_pandas.py
@@ -1,7 +1,11 @@
 #3.Display the first 5 rows of a DataFrame
+
 import pandas as pd
-data = {'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+
+data = {'Name': ['harshil', 'meet', 'fenil', 'viral', 'bhavy'],
         'Age': [25, 30, 35, 40, 45],
-        'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']}
+        'City': ['Nikol', 'hirawadi', 'india colony', 'naroda', 'sbr']}
+
 df = pd.DataFrame(data)
+
 print(df.head(3))       
