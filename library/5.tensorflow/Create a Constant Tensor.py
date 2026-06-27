5f4df817b6e0e7a6a2250f3f2b2671d632133025
Harshil <sorathiyaharshil97@gmail.com>
2026-06-27 11:53:33 +0530

    .

---
+++ library/5.tensorflow/Create a Constant Tensor.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+a =tf.constant(10)
+
+print(a)
+
+
---
+++ library/5.tensorflow/Different Types of Tensors.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+scalar = tf.constant(5)
+
+vector = tf.constant([1,2,3,4,])
+
+matrix = tf.constant([[1,2],[3,4]])
+
+print(scalar)
+print(vector)
+print(matrix)
