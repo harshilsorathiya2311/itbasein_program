bf3816d9cc489c4a46d16ab3aa2fc197049b53d4
Harshil <sorathiyaharshil97@gmail.com>
2026-07-30 11:58:46 +0530

    .

--- all programs/dictionary/3darray.py
+++ all programs/dictionary/3darray.py
@@ -13,3 +13,18 @@ arr = np.array([
 
 print(arr)
 print("dimension:", arr.ndim)
+
+
+
+cube = [
+    [
+        [1, 2, 3],
+        [4, 5, 6]
+    ],
+    [
+        [7, 8, 9],
+        [10, 11, 12]
+    ]
+]
+
+print(cube)
